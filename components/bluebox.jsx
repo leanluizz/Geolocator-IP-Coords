c9d6b7a8bac901d7e85df133feefe2db9823f6e5
Leandro Luiz <luizzleandro827@gmail.com>
2022-12-27 23:19:17 -0300

    add components and css files

---
+++ components/bluebox.jsx
@@ -0,0 +1,37 @@
+import {react, useEffect, useState} from "react";
+import box from "../styles/box.module.css";
+import Man from "../images/3dMan.png";
+import Image from "next/image";
+
+export default function (){
+
+    if(process.browser){
+        var [more, setmore] = useState(0)
+
+        const div = document.querySelectorAll("div");
+    
+   useEffect(() => { 
+    setTimeout(() => {
+            const animationBoxPerson = setInterval(() => {
+                setmore(more++)
+
+                div[7].style.opacity = `${more}%`
+                if (more == 100) {
+                    setmore(100)
+                 clearInterval(animationBoxPerson)
+                }
+             }, 10);
+        }, 20000);}, [])
+    }
+
+    return (
+        <div className={box.boxPerson}>
+        <Image src={Man}  className={box.gps} id={box.man}  alt="man" />  
+        <h1 className={box.h1Person}>Search Coordinates or IP </ h1>
+        <h2 className={box.h2Person}>Coordinates</h2>
+        <p  className={box.pPerson}>Coordinates A pair of numbers that describe the position of a point on a coordinate plane by using the horizontal and vertical distances from the two reference axes. Usually represented by (x,y) the x-value and y-value.</p>
+        <h2 className={box.h2Person}>IP Address</h2>
+        <p  className={box.pPerson}>An IP address is a unique address that identifies a device on the internet or a local network. IP stands for "Internet Protocol," which is the set of rules governing the format of data sent via the internet or local network.</p>
+       </div>
+    )
+}
---
+++ components/search.jsx
@@ -0,0 +1,21 @@
+import react from "react";
+import Search from "../styles/search.module.css"
+import Astronaut from "../images/astronaut.gif"
+import Image from "next/image"
+
+export default function (props){
+    return(
+        <div className={Search.box}>
+            <div className={Search.Earth}>
+                <Image src={Astronaut}/>
+                <div class={Search.list}>
+
+                </div>
+            </div>
+           <form className={Search.content_blue}>
+                <input type="text" className={Search.input} placeholder="Search ip or coordinates here!" />
+                <button className={Search.button}>Go!</button>
+           </form>
+        </div>
+    )
+}
